40b1cab7fc1e70445721082989d13c0f90b7a893
desh <deshcoldworld@gmail.com>
2025-03-15 20:57:33 +0100

    open graph set up

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,33 +2,35 @@
 
 import Providers from "@/app/provider";
 import Dropdown from "@/components/motion/dropdown";
-import { CalculationProvider } from '@/context/calculationContext';
 import "@/app/globals.css";
 
 export const metadata = {
   title: "Capital Rise - Asset Growth Calculator & Financial News",
-  description:
-    "Track asset growth with our smart calculator and stay updated with financial news. Capital Rise helps you forecast your wealth with ease.",
+  description: "Track asset growth with our smart calculator and stay updated with financial news. Capital Rise helps you forecast your wealth with ease.",
   keywords: "finance, investment, asset growth, financial news, wealth calculator",
   author: "Capital Rise Team",
   robots: "index, follow",
   openGraph: {
     title: "Capital Rise - Asset Growth Calculator & Financial News",
-    description:
-      "Track asset growth with our smart calculator and stay updated with financial news.",
-    url: "https://capitalrise.app",
+    description: "Track asset growth with our smart calculator and stay updated with financial news.",
+    url: "https://capitalrise.vercel.app",
     type: "website",
     images: [
       {
-        url: "https://capitalrise.app/og-image.jpg",
+        url: "https://res.cloudinary.com/dobwoeo5p/image/upload/v1742068124/capitalRise_binoie.jpg", // Cloudinary URL
         width: 1200,
         height: 630,
         alt: "Capital Rise Banner",
       },
     ],
   },
+  twitter: {
+    card: "summary_large_image", // Use "summary_large_image" for images
+    title: "Capital Rise - Asset Growth Calculator & Financial News",
+    description: "Track asset growth with our smart calculator and stay updated with financial news.",
+    images: ["https://res.cloudinary.com/dobwoeo5p/image/upload/v1742068124/capitalRise_binoie.jpg"], // Cloudinary URL
+  },
 };
-
 
 export default function RootLayout({ children }) {
   return (
